e275b16479f2a412b3be4004f3a1f5d13e0d52f0
Shashank <shashanklogan@gmail.com>
2021-06-19 20:58:46 +0530

    new changes

--- Deutschquiz/settings.py
+++ Deutschquiz/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,3 +139,13 @@ STATICFILES_DIRS =[
     BASE_DIR/'static',
     BASE_DIR/'quizes'/'static',
 ]
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
